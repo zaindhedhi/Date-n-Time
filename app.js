4a2019a37fb6f4bcbaa6bb4ffbf7635da9e65aba
zaindhedhi <zaindhedhi@oulook.com>
2024-08-11 21:41:20 +0500

    methods

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-var currentDate = new Date();
-var zainBirthDate = new Date('November 20, 2003');
+var rightNow = new Date('november 21, 2003');
+console.log(rightNow);
 
-var currentMillisecond = currentDate.getTime();
-var remainingsecond = zainBirthDate.getTime();
 
-console.log(currentMillisecond - remainingsecond / 1000 / 60 / 60 / 24 / 365)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var currentDate = new Date();
+// var zainBirthDate = new Date('November 20, 2003');
+
+// var currentMillisecond = currentDate.getTime();
+// var remainingsecond = zainBirthDate.getTime();
+
+// console.log(currentMillisecond - remainingsecond / 1000 / 60 / 60 / 24 / 365)
