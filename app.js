8ceebe62a794cdf0afd87fb920c168c7971d9338
zaindhedhi <zaindhedhi@oulook.com>
2024-08-12 07:24:35 +0500

    methods-completed

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var rightNow = new Date('november 21, 2003');
+// Current Date n Time methods
+var rightNow = new Date();
+
 console.log(rightNow);
 
+// for change in string
+console.log(rightNow.toString());
 
+// for day of weak
+console.log(rightNow.getDay())
 
+// for month 
+console.log(rightNow.getMonth());
+
+// for date of month
+console.log(rightNow.getDate());
+
+// for full year
+console.log(rightNow.getFullYear());
+
+// for get hours 
+console.log(rightNow.getHours());
+
+// for get minutes 
+console.log(rightNow.getMinutes());
+
+// for get seconds
+console.log(rightNow.getSeconds());
+
+// for get milli seconds
+console.log(rightNow.getMilliseconds());
+
+// for get Time since 01 Jan, 1970 in milliseconds 
+console.log(rightNow.getTime());
 
 
 
